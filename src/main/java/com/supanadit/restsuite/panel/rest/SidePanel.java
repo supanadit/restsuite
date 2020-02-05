754dc4bd4033d99890e4c72c8aac77beed094a52
Supan Adit Pratama <supanadit@gmail.com>
2020-02-05 22:24:01 +0700

    clear rest api when selection on parent collection

--- src/main/java/com/supanadit/restsuite/panel/rest/SidePanel.java
+++ src/main/java/com/supanadit/restsuite/panel/rest/SidePanel.java
@@ -44,6 +44,9 @@ public class SidePanel extends JScrollPane implements RestCallback {
                     CollectionEntity collectionEntity = collectionStructureEntity.getCollectionEntity();
                     restPanel.setData(collectionEntity);
                 }
+                if (selectedNode.getParent() == null) {
+                    restPanel.clear();
+                }
             }
         });
         tree.addMouseListener(new CollectionTreeMouseMenuListener(this));
